9a09b72e6fd1e6019b8cd7991ca45ed37ba9c827
Matthew Scott <gldnspud@gmail.com>
2011-06-14 12:05:27 -0700

    More concise setting of publisher title.

--- cdf/publisher.py
+++ cdf/publisher.py
@@ -3,7 +3,4 @@ class Publisher(object):
     def __init__(self, name, configitems):
         self.name = name
         configitems = dict(configitems)
-        if 'title' in configitems:
-            self.title = configitems['title']
-        else:
-            self.title = name
+        self.title = configitems.pop('title', name)
